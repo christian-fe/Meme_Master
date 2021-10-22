46707adde2eae5627c92bb8d61ab216304159a8e
christian-fe <christian.se@globant.com>
2021-10-22 09:01:38 -0500

    Category to category

---
+++ Data/Migrations/20211022022617_Photo_Dbo.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Memes.Migrations
+{
+    public partial class Photo_Dbo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MemeName",
+                table: "Photo",
+                newName: "PhotoName");
+
+            migrationBuilder.RenameColumn(
+                name: "MemeId",
+                table: "Photo",
+                newName: "PhotoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PhotoName",
+                table: "Photo",
+                newName: "MemeName");
+
+            migrationBuilder.RenameColumn(
+                name: "PhotoId",
+                table: "Photo",
+                newName: "MemeId");
+        }
+    }
+}
